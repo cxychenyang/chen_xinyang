60d2050f81b855060e77f7aa7309b9238cd93027
DESKTOP-BQAL504\rusty <1695712839@qq.com>
2019-03-25 23:29:44 +0800

    1701020404，陈新洋matrix类头文件与类定义

--- chapter.10/Matrix.cpp
+++ chapter.10/Matrix.cpp
@@ -1,33 +1,280 @@
-#include<iostream>
-#include<iomanip>
-#include<cmath>
-#include<string>
-#include"Matrix.H"
-ostream &operator<<( ostream &output,const Matrix &number )
+#include "Matrix.h"
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+Matrix::Matrix(int m, int n)
 {
-    output<<number.elements1<<number.elements2<<number.elements3<<"\n"<<number.elements4<<number.elements5<<number.elements6
-    <<"\n"<<number.elements7<<number.elements8<<number.elements9<<endl;
-     return output;
+    if (m < 0 || n < 0)
+    {
+        cout << "矩阵大小不能为负\n";
+        return;
+    }
+    rowNum = m;
+    colNum = n;
+    item = new double[m*n];
+    for (int i = 0; i < m*n; i++)
+    {
+        item[i] = 0;
+    }
 }
-istream &operator>>(istream &input,Matrix &number)
+
+
+Matrix::Matrix(double* items, int m, int n)
 {
-     input>>setw(1)>>number.elements1;
-     input.ignore(1);
-     input>>setw(1)>>number.elements2;
-     input.ignore(1);
-     input>>setw(1)>>number.elements3;
-     input.ignore(1);
-     input>>setw(1)>>number.elements4;
-     input.ignore(1);
-     input>>setw(1)>>number.elements5;
-     input.ignore(1);
-     input>>setw(1)>>number.elements6;
-     input.ignore(1);
-     input>>setw(1)>>number.elements7;
-     input.ignore(1);
-     input>>setw(1)>>number.elements8;
-     input.ignore(1);
-     input>>setw(1)>>number.elements9;
-     input.ignore(1);
-     return input;
+    rowNum = m;
+    colNum = n;
+    item = new double[m*n];
+    for (int i = 0; i < colNum*rowNum; i++)
+    {
+        item[i] = items[i];
+    }
+}
+Matrix::Matrix(int n)
+{
+    rowNum = colNum = n;
+    item = new double[n*n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+                set(i, j, 1.0);
+            else
+                set(i, j, 0);
+        }
+    }
+}
+Matrix::Matrix(const Matrix &M)
+{
+    colNum = M.colNum;
+    rowNum = M.rowNum;
+
+    item = new double[colNum*rowNum];
+    for (int i = 0; i < colNum*rowNum; i++)
+    {
+        item[i] = M.item[i];
+    }
+}
+Matrix& Matrix::operator=(const Matrix & M)
+{
+    colNum = M.colNum;
+    rowNum = M.rowNum;
+    if (item != nullptr) delete[] item;
+    item = new double[colNum*rowNum];
+    for (int i = 0; i < colNum*rowNum; i++)
+    {
+        item[i] = M.item[i];
+    }
+    return *this;
+}
+
+Matrix Matrix::FromFile(std::string file)
+{
+    ifstream read(file);
+    if (!read.is_open())
+    {
+        cout << "Matrix::未能打开文件\n";
+    }
+    int rows = 0;
+    string line;
+    vector<double> nums;
+    while (getline(read, line))
+    {
+        istringstream record(line);
+        double num = 0.0;
+        while (record >> num)  nums.push_back(num);
+        rows++;
+    }
+    return Matrix(&(*nums.begin()), rows, nums.size() / rows);
+}
+
+Matrix::~Matrix()
+{
+    delete[] item;
+}
+double Matrix::get(int i, int j) const
+{
+    return item[i*colNum + j];
+}
+void Matrix::set(int i, int j, double value)
+{
+    item[i*colNum + j] = value;
+}
+void Matrix::RowSwap(int i, int j, double multiply)
+{
+    if (j == -1)
+    {
+        for (int k = 0; k < colNum; k++)
+        {
+            set(i, k, multiply*get(i, k));
+        }
+    }
+    else
+    {
+        for (int k = 0; k < colNum; k++)
+        {
+            set(j, k, multiply*get(i, k) + get(j, k));
+        }
+    }
+}
+void Matrix::RowSwap(int i, int j)
+{
+    Matrix _copy = *this;
+    for (int k = 0; k < colNum; k++)
+    {
+        double swap = _copy.get(j, k);
+        set(j, k, _copy.get(i, k));
+        set(i, k, swap);
+    }
+}
+Matrix Matrix::Trans() const
+{
+    Matrix _copy = *this;
+    _copy.rowNum = this->colNum;
+    _copy.colNum = this->rowNum;
+    for (int i = 0; i < _copy.rowNum; i++)
+    {
+        for (int j = 0; j < _copy.colNum; j++)
+        {
+            _copy.set(i, j, get(j, i));
+        }
+    }
+    return _copy;
+}
+int Matrix::getRowNum() const
+{
+    return rowNum;
+}
+int Matrix::getColNum() const
+{
+    return colNum;
+}
+ostream& operator <<(ostream &os, const Matrix &m)
+{
+    for (int i = 0; i < m.rowNum; i++)
+    {
+        for (int j = 0; j < m.colNum; j++)
+            os << std::setw(10) << std::fixed << std::setprecision(12) << m.get(i, j) << " ";
+        os << "\n";
+    }
+    os.flush();
+    return os;
+}
+Matrix Matrix::operator +(const Matrix &m)
+{
+    if (m.colNum != colNum || m.rowNum != rowNum) return *this;
+    Matrix _copy = *this;
+    for (int i = 0; i < rowNum; i++)
+    {
+        for (int j = 0; j < colNum; j++)
+        {
+            _copy.set(i, j, get(i, j) + m.get(i, j));
+        }
+    }
+    return _copy;
+}
+Matrix Matrix::operator -(const Matrix &m)
+{
+    if (m.colNum != colNum || m.rowNum != rowNum) return *this;
+    Matrix _copy = *this;
+    for (int i = 0; i < rowNum; i++)
+    {
+        for (int j = 0; j < colNum; j++)
+        {
+            _copy.set(i, j, get(i, j) - m.get(i, j));
+        }
+    }
+    return _copy;
+}
+Matrix Matrix::operator *(const double f)
+{
+    Matrix _copy = *this;
+    for (int i = 0; i < rowNum; i++)
+    {
+        for (int j = 0; j < colNum; j++)
+        {
+            _copy.set(i, j, get(i, j)*f);
+        }
+    }
+    return _copy;
+}
+Matrix Matrix::operator *(const Matrix &m)
+{
+    if (colNum != m.rowNum)
+    {
+        cout << "无法相乘！";
+        return *this;
+    }
+    Matrix _copy(rowNum, m.getColNum());
+    for (int i = 0; i < rowNum; i++)
+    {
+        for (int j = 0; j < m.colNum; j++)
+        {
+            double sum = 0;
+            for (int k = 0; k < m.rowNum; k++)
+            {
+                sum += get(i, k)*m.get(k, j);
+            }
+            _copy.set(i, j, sum);
+        }
+    }
+    return _copy;
+}
+Matrix Matrix::Inverse()
+{
+    Matrix _copy = *this;
+
+    Matrix result(colNum);
+    if (colNum != rowNum)
+    {
+        cout << "矩阵不可逆！" << endl;
+        return *this;
+    }
+    for (int i = 0; i < rowNum; i++)
+    {
+        int MaxRow = i;
+
+        double max = abs(_copy.get(i, i));
+        for (int j = i; j < colNum; j++)
+        {
+            if (abs(_copy.get(j, i))>max)
+            {
+                max = abs(_copy.get(j, i));
+                MaxRow = j;
+            }
+        }
+
+        if (MaxRow != i)
+        {
+            result.RowSwap(i, MaxRow);
+            _copy.RowSwap(i, MaxRow);
+        }
+
+        double r = 1.0 / _copy.get(i, i);
+        _copy.RowSwap(i, -1, r);
+        result.RowSwap(i, -1, r);
+
+        for (int j = 0; j < rowNum; j++)
+        {
+            if (j == i) continue;
+            r = -_copy.get(j, i);
+            _copy.RowSwap(i, j, r);
+            result.RowSwap(i, j, r);
+        }
+    }
+
+    return result;
+}
+void Matrix::FlowOver()
+{
+    for (int i = 0; i < rowNum;i++)
+    {
+        for (int j = 0; j < colNum;j++)
+        {
+            if (abs(get(i, j)) <= OVERFLOWED) set(i, j, 0);
+        }
+    }
 }
